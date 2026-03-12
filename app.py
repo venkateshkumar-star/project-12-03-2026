6ba34ae2164e6612d1c3330c8d496be6da975766
venkateshkumar-star <rvenkateshkumar2605@gmail.com>
2026-03-13 01:12:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "DevOps CI/CD Pipeline Running!"
+    return "DevOps CI/CD Pipeline Running Successfully by Venkat!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
